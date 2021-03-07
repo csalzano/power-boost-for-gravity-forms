b2bd79dc4ad84a98d784a4cb8f25ba2ee5574ad3
Corey Salzano <corey.salzano@gmail.com>
2021-03-07 08:09:04 -0500

    Adds a feature. Now highlights the names of forms in the recent forms list under Forms in the admin bar if the form is included on the page the user is looking at. Changes the text color right now, but a glyph of some sort should also be used for accessibility for color blindness.

--- gravityforms-power-boost.php
+++ gravityforms-power-boost.php
@@ -11,6 +11,8 @@ defined( 'ABSPATH' ) or exit;
 
 class Gravity_Forms_Power_Boost
 {
+	var $rendered_form_ids;
+
 	public function add_columns_to_list_table( $columns )
 	{
 		$columns['last_entry'] = esc_html__( 'Last Entry', 'gravityforms' );
@@ -24,6 +26,85 @@ class Gravity_Forms_Power_Boost
 
 		//Populate the columns we added to the list table
 		add_action( 'gform_form_list_column_last_entry', array( $this, 'populate_columns_we_added' ), 10, 1 );
+
+		//Keep track of all Gravity Forms form IDs that are rendered during this request
+		add_action( 'gform_pre_render', array( $this, 'save_rendered_form_ids' ), 10, 3 );
+		
+		//Change the Forms menu of the admin bar
+		add_action( 'wp_before_admin_bar_render', array( $this, 'enhance_admin_bar' ), 99 );
+
+		//Include a style sheet to customize the admin bar
+		add_action( 'wp_enqueue_scripts', array( $this, 'include_admin_bar_css' ) );
+	}
+
+	public function include_admin_bar_css()
+	{
+		if( ! is_admin_bar_showing() )
+		{
+			return;
+		}
+		wp_enqueue_style( 'gfpb-admin-bar', plugins_url( 'admin-bar.min.css', __FILE__ ) );
+	}
+	
+	/**
+	 * save_rendered_form_ids
+	 * 
+	 * Hook callback on gform_pre_render. Just before a Gravity Form is 
+	 * rendered, save its form ID in a member variable in this class so we know
+	 * which forms are on the page.
+	 *
+	 * @param  mixed $form
+	 * @param  mixed $is_ajax
+	 * @param  mixed $field_values
+	 * @return void
+	 */
+	public function save_rendered_form_ids( $form, $is_ajax, $field_values )
+	{
+		if( empty( $form['id'] ) )
+		{
+			return;
+		}
+
+		if( ! is_array( $this->rendered_form_ids ) )
+		{
+			$this->rendered_form_ids = array();
+		}
+		$this->rendered_form_ids[] = $form['id'];
+	}
+
+	public function enhance_admin_bar()
+	{
+		global $wp_admin_bar;
+
+		//Settings for adding a link to main GF settings
+		//https://docs.gravityforms.com/role-management-guide/#gravity-forms-capabilities
+		//gravityforms_view_settings 	View Plugin Settings
+		//gravityforms_edit_settings 	Edit Plugin Settings
+
+		foreach( $wp_admin_bar->get_nodes() as &$node )
+		{
+			//Is this node a form in the Recent forms menu?
+			if( ! empty( $node->parent ) && 'gform-form-recent-forms' == $node->parent )
+			{
+				$form_id = intval( str_replace( 'gform-form-', '', $node->id ) );
+				if( in_array( $form_id, $this->rendered_form_ids ) )
+				{
+					if( ! is_array( $node->meta ) )
+					{
+						$node->meta = array();
+					}
+					if( ! isset( $node->meta['class'] ) )
+					{
+						$node->meta['class'] = '';
+					}
+
+					$node->meta['class'] .= ' gfpb-recent';
+					$node->meta['class'] = trim( $node->meta['class'] );
+				}
+				$wp_admin_bar->remove_node( $node->id );
+				$wp_admin_bar->add_node( $node );
+			}
+		}
 	}
 
 	/**
